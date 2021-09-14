a997fd74b54b964af68f4c65819a9672aa0377be
tuituidan <136375002@qq.com>
2021-09-14 22:10:01 +0800

    明确指定上传文件的临时目录，避免Tomcat使用默认的tmp而被操作系统清理掉

---
+++ tresdin-core-starter/src/main/java/com/tuituidan/tresdin/config/AppConfig.java
@@ -0,0 +1,37 @@
+package com.tuituidan.tresdin.config;
+
+import java.io.File;
+import javax.servlet.MultipartConfigElement;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.Assert;
+
+/**
+ * AppConfig.
+ *
+ * @author tuituidan
+ * @version 1.0
+ * @date 2021/9/14
+ */
+@Configuration
+public class AppConfig {
+
+    /**
+     * 明确指定上传文件的临时目录，避免Tomcat使用默认的tmp而被操作系统清理掉
+     *
+     * @return MultipartConfigElement
+     */
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        String location = System.getProperty("user.dir") + "/temp";
+        File tmpFile = new File(location);
+        if (!tmpFile.exists()) {
+            Assert.isTrue(tmpFile.mkdirs(), "临时上传路径创建失败");
+        }
+        factory.setLocation(location);
+        return factory.createMultipartConfig();
+    }
+
+}
