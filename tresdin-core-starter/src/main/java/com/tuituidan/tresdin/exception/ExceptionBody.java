1321f7c7c012afdeb781d7670fecfe0319383122
tuituidan <tuituidan@163.com>
2022-10-18 20:31:59 +0800

    默认异常处理

---
+++ tresdin-core-starter/src/main/java/com/tuituidan/tresdin/exception/ExceptionBody.java
@@ -0,0 +1,38 @@
+package com.tuituidan.tresdin.exception;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * ExceptionBody.
+ *
+ * @author tuituidan
+ * @version 1.0
+ * @date 2022/10/16
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+public class ExceptionBody implements Serializable {
+
+    private static final long serialVersionUID = 4127127070319148802L;
+
+    /**
+     * ExceptionBody
+     *
+     * @param status status
+     * @param message message
+     */
+    public ExceptionBody(HttpStatus status, String message) {
+        this.code = status.value();
+        this.message = message;
+    }
+
+    private int code;
+
+    private String message;
+
+}
